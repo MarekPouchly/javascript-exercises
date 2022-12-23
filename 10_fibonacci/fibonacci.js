26d5cb1c0a1e0990a923cc5c2473adfc6c8b751a
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-23 19:36:17 +0100

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
+    if (isNaN(number)){
+        number = stoi(number)
+    }
+    
+    if (number >= 0) {
+        for (let i = 1; i < number; i++) {
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+    } else if (number < 0) {
+        return "OOPS"
+    }
+    return nextTerm
 };
 
 // Do not edit below this line
