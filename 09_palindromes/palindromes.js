68f785844b4807972e47ce28c9393c00bbfdfde5
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-23 18:43:34 +0100

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    let removeSpecialChars = string.split(/[\s,\.!]+/)
+    let joinChars = removeSpecialChars.join("");
+    let splitChars = joinChars.split("")
+    let frontPalindrome = splitChars.join("").toLowerCase();
 
+    let reversePalindrome = splitChars.reverse();
+    let backPalindrome = reversePalindrome.join("").toLowerCase();
+
+    if (frontPalindrome === backPalindrome) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
