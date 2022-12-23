e6da162a6869fbb9584df09c0a375cec79258e6e
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-23 15:48:23 +0100

    Progressing on exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+let returnedString
 
+const repeatString = function(word,number) {
+    returnedString = '';
+    if (number >= 0) {
+        for(let i=0; i<number; i++) {
+            returnedString += `${word}`;
+        }
+        return returnedString;
+    } else if (number < 0) {
+        return 'ERROR';
+    }
 };
+
+console.log(returnedString)
 
 // Do not edit below this line
 module.exports = repeatString;
