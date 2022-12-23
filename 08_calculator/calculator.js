f5ad3680ff75833abe651f0ea89a896b7ce1a043
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-23 17:49:31 +0100

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	let result = numOne + numTwo;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	let result = numOne - numTwo;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array[i]
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i]
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+  let result = numOne ** numTwo
+	return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) { 
+    return 1; 
+  } else { 
+    return num * factorial( num - 1 ); 
+  }
 };
 
 // Do not edit below this line
