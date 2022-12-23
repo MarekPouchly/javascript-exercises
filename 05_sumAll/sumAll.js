70a3cbbca7764517c4293dc7cd6705fcae15c4e8
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-23 16:53:03 +0100

    Exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let sum = 0
+    
+    if (numOne < 0 || numTwo < 0) {
+        return 'ERROR';
+    } else if (typeof numOne != 'number' || typeof numTwo != 'number') {
+        return 'ERROR'
+    } else if (isNaN(numOne) || isNaN(numTwo)) {
+        return 'ERROR'
+    }
+    
+    for (i = numOne; (numOne < numTwo) ? i <= numTwo : i >= numTwo; (numOne < numTwo) ? i++ : i--) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
