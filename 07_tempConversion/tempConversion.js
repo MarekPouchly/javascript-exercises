e6fce520682339808d62cce675d8a31cd6f6f731
Marek Pouchlý <marekpouchl8gmail.com>
2022-12-23 17:19:56 +0100

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature = (temperature - 32) * .5556;
+  let rounded = Math.round(temperature * 10) / 10
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature = (temperature * 1.8) + 32
+  let rounded = Math.round(temperature * 10) / 10
+  return rounded;
 };
 
 // Do not edit below this line
